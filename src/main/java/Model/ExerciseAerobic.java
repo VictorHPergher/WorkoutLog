fc2cb2e1c1db8574ca9404b0b7a653abdd0af662
pergher <pergher@pergher_pc>
2017-11-06 18:18:58 -0200

    ExerciseAerobicBuilder

--- src/main/java/Model/ExerciseAerobic.java
+++ src/main/java/Model/ExerciseAerobic.java
@@ -10,10 +10,33 @@ import java.util.ArrayList;
 public class ExerciseAerobic extends Exercise {
 
 	private ArrayList<Interval> intervals;
+        
+        private ExerciseAerobic(ExerciseAerobicBuilder builder) {
+            this.title = builder.title;
+            this.description = builder.description;
+            this.equipment = builder.equipment;
+            this.intervals = builder.intervals;
+        }
 
 	public ArrayList<Interval> getIntervals() {
 		return intervals;
 
 	}
+        
+        public static class ExerciseAerobicBuilder {
 
+        private ArrayList<Interval> intervals;
+        private String title, description, equipment; //description optional
+
+        public ExerciseAerobicBuilder(String title, String equipment, ArrayList<Interval> intervals) {
+            this.title = title;
+            this.equipment = equipment;
+            this.intervals = intervals;
+        }
+
+        public ExerciseAerobicBuilder addDescription(String description) {
+            this.description = description;
+            return this;
+        }
+    }
 }
